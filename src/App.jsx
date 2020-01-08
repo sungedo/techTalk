b236038100581e57127e71e64b7e20419bf71d8b
unknown <sungdo1004@gmail.com>
2020-01-08 13:03:14 -0800

    changed some components

--- src/App.jsx
+++ src/App.jsx
@@ -6,49 +6,50 @@ import Topics from './Topics'
 import Homepage from './Homepage'
 import { Route, Link, Switch, BrowserRouter as Router } from 'react-router-dom'
 
-// class Testing extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = { currentView: "home" };
-//   }
-//   render() {
-//     const currentView = this.state.currentView;
-//     return (
-//       <div class="App-header">
-//         <ul>
-//           <li onClick={() => this.setState({ currentView: "home" })}>Home</li>
-//           <li onClick={() => this.setState({ currentView: "about" })}>About</li>
-//           <li onClick={() => this.setState({ currentView: "topics" })}>Topics</li>
-//         </ul>
-//         {currentView === "home" && <Home />}
-//         {currentView === "about" && <About />}
-//         {currentView === "topics" && <Topics />}
-//       </div>
-//     );
-//   }
-// }
-
 class Testing extends React.Component {
+  constructor() {
+    super();
+    this.state = { currentView: "home" };
+  }
   render() {
+    const currentView = this.state.currentView;
     return (
-      <Router>
       <div class="App-header">
         <ul>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/topics">Topics</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li onClick={() => this.setState({ currentView: "about" })}>About</li>
+          <li onClick={() => this.setState({ currentView: "topics" })}>Topics</li>
+          <li onClick={() => this.setState({ currentView: "contact" })}>Contact</li>
         </ul>
-        <Switch>
-          <Route path="/contact" component={Contact}/>
-          <Route path="/about" component={About}/>
-          <Route path="/topics" component={Topics}/>
-          <Homepage/>
-        </Switch>
+        {currentView === "contact" && <Contact />}
+        {currentView === "about" && <About />}
+        {currentView === "topics" && <Topics />}
+        {currentView === 'home' && <Homepage/>}
       </div>
-    </Router>
-    )
+    );
   }
 }
+
+// class Testing extends React.Component {
+//   render() {
+//     return (
+//       <Router>
+//       <div class="App-header">
+//         <ul>
+//           <li><Link to="/about">About</Link></li>
+//           <li><Link to="/topics">Topics</Link></li>
+//           <li><Link to="/contact">Contact</Link></li>
+//         </ul>
+//         <Switch>
+//           <Route path="/contact" component={Contact}/>
+//           <Route path="/about" component={About}/>
+//           <Route path="/topics" component={Topics}/>
+//           <Homepage/>
+//         </Switch>
+//       </div>
+//     </Router>
+//     )
+//   }
+// }
 
 export default Testing
 
